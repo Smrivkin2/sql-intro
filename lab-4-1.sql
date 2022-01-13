f2dc1a11cf6bf62c4869483e2b2ad1dbf179a4a6
Smrivkin2 <stephanie.rivkin@kellogg.northwestern.edu>
2022-01-13 03:31:52 +0000

    Everything from Class 2

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,9 @@
 -- Expected result:
 -- 2935
 
+SELECT first_name, last_name, SUM(hits)
+FROM stats 
+INNER JOIN players ON stats.id = players.id 
+WHERE first_name = “Barry” 
+and last_name = “Bonds”;
 
---
+++ school-8-insert-again.sql
@@ -0,0 +1,9 @@
+INSERT INTO students (
+    first_name,
+    last_name,
+    email
+) VALUES (
+    "Stephanie",
+    "Rivkin",
+    "stephanie.m.rivkin@gmail.com"
+);
